a9a0302f11da3f1b51f8173193fd63d801f20b76
Manas <manas18244@iiitd.ac.in>
2021-08-12 00:10:36 +0530

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,134 @@
+#![allow(dead_code)]
+/// A generic quicksorting algorithm
+/// Here term 'generic' refers to any type of objects where a partial order can
+/// be established will be sorted.
+use std::cmp::Ordering;
+
+fn partition(arr: &mut Vec<i64>, low: usize, high: usize) -> usize {
+    let pivot = arr[high - 1];
+    let mut idx = low;
+
+    for j in low..high {
+        if arr[j] <= pivot {
+            arr.swap(idx, j);
+            idx = idx + 1;
+        }
+    }
+    idx
+}
+
+fn quicksort(arr: &mut Vec<i64>, low: usize, high: usize) -> &Vec<i64> {
+    if low >= usize::MIN && high >= usize::MIN {
+        if low < high {
+            let mid = partition(arr, low, high);
+            quicksort(arr, low, mid - 1);
+            quicksort(arr, mid, high);
+        }
+    }
+    arr
+}
+
+pub(crate) fn sort(arr: &mut Vec<i64>) -> &Vec<i64> {
+    quicksort(arr, usize::MIN, arr.len())
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Generic implementation of quicksort (sort_gen)
+
+/// Defined a partially-ordered comparator to be used to compare objects while
+/// sorting.
+trait Comparator {
+    fn compare(&self, other: &Self) -> Ordering;
+}
+
+/// A cloning trait for moving objects when they are mutable.
+trait Copier {
+    fn copy(&self) -> Self;
+}
+
+fn partition_gen<T: Comparator + Copier>(arr: &mut Vec<T>, low: usize, high: usize) -> usize {
+    let pivot: T = T::copy(&arr[high - 1]);
+    let mut idx = low;
+
+    for j in low..high {
+        if T::compare(&arr[j], &pivot) == Ordering::Less {
+            arr.swap(idx, j);
+            idx = idx + 1;
+        }
+    }
+    idx
+}
+
+fn quicksort_gen<'a, T: Comparator + Copier>(
+    arr: &'a mut Vec<T>,
+    low: usize,
+    high: usize,
+) -> &'a Vec<T> {
+    if low >= usize::MIN && high >= usize::MIN {
+        if low < high {
+            let mid = partition_gen(arr, low, high);
+            quicksort_gen(arr, low, mid - 1);
+            quicksort_gen(arr, mid, high);
+        }
+    }
+    arr
+}
+
+pub(crate) fn sort_gen<'a, T: Comparator + Copier>(arr: &'a mut Vec<T>) -> &'a Vec<T> {
+    quicksort_gen(arr, usize::MIN, arr.len())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::Rng;
+
+    /// Represents a process node
+    #[derive(Debug)]
+    struct Node {
+        pid: u64,
+        name: String,
+    }
+
+    impl Node {
+        fn new(p: u64, n: String) -> Self {
+            Self { pid: p, name: n }
+        }
+    }
+
+    impl Comparator for Node {
+        fn compare(&self, other: &Self) -> Ordering {
+            if self.pid > other.pid || self.name > other.name {
+                return Ordering::Greater;
+            }
+            return Ordering::Less;
+        }
+    }
+
+    impl Copier for Node {
+        fn copy(&self) -> Self {
+            Node {
+                pid: self.pid,
+                name: self.name.clone(),
+            }
+        }
+    }
+
+    #[test]
+    fn test_quicksort() {
+        let mut rng = rand::thread_rng();
+
+        let mut numbers: Vec<i64> = (0..25).map(|_| rng.gen_range(1..3001)).collect();
+        println!("{:?}", sort(&mut numbers));
+    }
+
+    #[test]
+    fn test_generic_quicksort() {
+        let mut nodes = vec![
+            Node::new(4, String::from("kobj2")),
+            Node::new(2, String::from("kobj")),
+            Node::new(1, String::from("systemd")),
+        ];
+        println!("{:?}", sort_gen(&mut nodes));
+    }
+}
